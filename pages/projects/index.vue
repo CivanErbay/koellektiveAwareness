f741b71f7b0b19a6e94b629e229a295ed8dc371f
FloMingo808 <el_flow@web.de>
2025-11-15 15:59:33 +0100

    Hinzugefuegt: Workshops 3 + 4

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -38,104 +38,175 @@
             <p>Wir haben gute Neuigkeiten!</p>
 
             <p>
-              Da das Interesse und die Nachfrage zu unserem im Jahr 2024 angebotenen, kostenfreien Einführungsworkshop
-              zu Awareness bereits hoch war, können wir diese Grundlagen-Workshops für die freie Kulturszene in Köln in
-              einer überarbeiteten und aktualisierten Fassung erneut anbieten!
+              Da das Interesse und die Nachfrage zu unserem im Jahr 2024 angebotenen, kostenfreien
+              Einführungsworkshop zu Awareness bereits hoch war, können wir diese
+              <strong>Grundlagen-Workshops für die freie Kulturszene in Köln</strong>
+              in einer überarbeiteten und aktualisierten Fassung erneut anbieten!
             </p>
 
-            <p class="italic text-base text-primary-600 dark:text-primary-300">
-              Ausblick: In der zweiten Jahreshälfte 2025 werden darauf aufbauende Workshops angeboten.
-            </p>
-
-            <p>
-              Gefördert wird die insgesamt vierteilige Workshopreihe von der Stadt Köln im Programm „Kultur –
-              Diversity“. Die Teilnahme ist daher kostenfrei!
-              Das Projekt ist eine gemeinsame Initiative der Köllektive Awareness (Verein in Gründung), der KLUBKOMM –
-              Verband Kölner Clubs und Veranstalter*innen e. V. und ArtAsyl e. V. / Studio Audience.
+            <p class="italic text-base text-[#ec0054]"><strong>
+              Ausblick: In der zweiten Jahreshälfte 2025 werden darauf aufbauende Workshops angeboten. Scrollt für die weiteren Angebote gerne weiter runter.
+              </strong>
             </p>
 
             <p>
-              Du interessierst dich für die Grundlagen und die Unterstützungsarbeit von Awareness im
-              Veranstaltungskontext? Du willst wissen, wie sicherere Orte erschaffen werden können in Theatern, freien
-              Kunsträumen, Tanzschulen, Konzertsälen, Museen, Musikschulen, für Literatur- & Spoken-Word-Formate,
-              Performancegruppen, Film- & Medienkollektive und Soziokulturelle Zentren & Community Spaces? Dann sind
-              unsere zwei Awareness-Einführungsworkshops die optimale Gelegenheit.
+              Das Projekt ist eine gemeinsame Initiative der Köllektive Awareness (Verein in Gründung), der KLUBKOMM – Verband Kölner Clubs und Veranstalter*innen e. V. (<a class="underline font-bold" href="https://www.klubkomm.de/">Klubkomm – Interessenverband der Kölner Clubs</a>) und ArtAsyl e. V. / Studio Audience (<a class="underline font-bold" href="https://www.artasyl.de/">ArtAsyl - Kunst verbindet Kulturen</a>).
+            </p>
+
+            <p>
+              Du interessierst dich für  die Grundlagen und die Unterstützungsarbeit von Awareness im Veranstaltungskontext?<br>
+              Du willst wissen, wie sicherere Orte erschaffen werden können in Theatern, freien Kunsträumen, Tanzschulen, Konzertsälen, Museen, Musikschulen, für Literatur- & Spoken-Word-Formate, Performancegruppen, Film- & Medienkollektive und Soziokulturelle Zentren & Community Spaces?<br>
+              Du befindest dich am Anfang einer Auseinandersetzung in eurer Einrichtung oder eurem Projekt?<br>
+              Dann sind unsere zwei Awareness-Einführungsworkshops die optimale Gelegenheit.
             </p>
 
             <!-- Workshop 1 -->
-            <div class="space-y-3">
+            <div style="margin-top: 120px;"  class="text-left space-y-3">
               <h4 class="text-xl font-bold mt-10">1. Workshop: Grundlagen von Awareness im Veranstaltungskontext</h4>
-              <p><strong>Zielgruppe:</strong> Teilnehmende aus der freien Kulturszene in Köln, die bisher keine
-                Awareness-Schulungen erhalten haben.</p>
-              <p><strong>Themenauszug:</strong><br>
-                Was umfasst Awareness und wo liegen die Grenzen?<br>
-                Vermittlung der Grundprinzipien von Awareness.<br>
-                Privilegien und Diskriminierung im Veranstaltungskontext.
-              </p>
+              <p><strong>Zielgruppe:</strong> Teilnehmende aus der freien Kulturszene in Köln, die bisher keine Awareness Schulungen erhalten haben.</p>
+              <p><strong>Themenauszug:</strong></p>
+              <ul class="m-0 !mt-0 !mb-0 list-disc ml-6">
+                <li>Was umfasst Awareness und wo liegen die Grenzen?</li>
+                <li>Vermittlung der Grundprinzipien von Awareness.</li>
+                <li>Privilegien und Diskriminierung im Veranstaltungskontext.</li>
+              </ul>
               <p><strong>Wann?</strong> Sa., 21.06.2025, 11 – 17 Uhr</p>
               <p><strong>Wo?</strong> Wohngemeinschaft, Richard-Wagner-Str. 39, 50674 Köln</p>
               <p><strong>Barriereinformationen:</strong><br>
                 Die Veranstaltung findet in deutscher Sprache statt.<br>
                 Die Räumlichkeiten sind für Rollstuhlnutzende zugänglich.<br>
-                Eine Verdolmetschung in Deutsche Gebärdensprache, Induktionstechnik oder Kinderbetreuung können wir
-                leider nicht anbieten.
+                Eine Verdolmetschung in Deutsche Gebärdensprache, Induktionstechnik oder Kinderbetreuung können wir leider nicht anbieten.
               </p>
-              <p><strong>Anmeldung:</strong> bis zum 18.06.2025 per E-Mail an <a class="underline font-bold"
+              <p><strong><u>Anmeldung:</u></strong> bis zum 18.06.2025 per E-Mail an <a class="underline font-bold"
                   href="mailto:info@koellektive-awareness.de">info@koellektive-awareness.de</a></p>
             </div>
 
             <!-- Workshop 2 -->
-            <div class="space-y-3">
+            <div style="margin-top: 120px;" class="text-left space-y-3">
               <h4 class="text-xl font-bold mt-10">2. Workshop: Unterstützungsarbeit von Awareness im
                 Veranstaltungskontext</h4>
               <p><strong>Zielgruppe:</strong> Teilnehmende aus der freien Kulturszene in Köln, die bisher keine
                 Awareness-Schulungen erhalten haben.</p>
-              <p><strong>Themenauszug:</strong><br>
-                Ablauf von Unterstützungsarbeit<br>
-                Aspekte in der Verweisberatung<br>
-                Szenariotraining<br>
-                Self Care
-              </p>
+              <p><strong>Themenauszug:</strong></p>
+              <ul class="m-0 !mt-0 !mb-0 list-disc ml-6">
+                <li>Ablauf von Unterstützungsarbeit</li>
+                <li>Aspekte in der Verweisberatung</li>
+                <li>Szenariotraining</li>
+                <li>Self Care</li>
+              </ul>
               <p><strong>Wann?</strong> Sa., 28.06.2025, 11 – 17 Uhr</p>
               <p><strong>Wo?</strong> Wohngemeinschaft, Richard-Wagner-Str. 39, 50674 Köln</p>
               <p><strong>Barriereinformationen:</strong><br>
                 Die Veranstaltung findet in deutscher Sprache statt.<br>
                 Die Räumlichkeiten sind für Rollstuhlnutzende zugänglich.<br>
-                Eine Verdolmetschung in Deutsche Gebärdensprache, Induktionstechnik oder Kinderbetreuung können wir
-                leider nicht anbieten.
+                Eine Verdolmetschung in Deutsche Gebärdensprache, Induktionstechnik oder Kinderbetreuung können wir leider nicht anbieten.
               </p>
-              <p><strong>Anmeldung:</strong> bis zum 13.06.2025 per E-Mail an <a class="underline font-bold"
+              <p><strong><u>Anmeldung:</u></strong> bis zum 13.06.2025 per E-Mail an <a class="underline font-bold"
                   href="mailto:info@koellektive-awareness.de">info@koellektive-awareness.de</a></p>
               <p>Wir freuen uns über zahlreiches Erscheinen.</p>
             </div>
 
-            <!-- Outlook -->
-            <div class="space-y-3 mt-10">
-              <h4 class="text-xl font-bold">Ausblick auf weitere Workshops</h4>
-              <p>
-                Im Zuge der diesjährigen Förderung planen wir zwei weitere Workshops, die im September / Oktober 2025
-                stattfinden werden. Auch diese werden von der Stadt Köln im Programm „Kultur – Diversity“ gefördert und
-                sind daher ebenfalls kostenlos.
-              </p>
-              <p>
-                Detaillierte Informationen und Anmeldemöglichkeiten werden wir ebenfalls hier auf unserer Internetseite
-                bekanntgeben.
-              </p>
-              <p>
-                Hier soll es zum einen darum gehen, wie Awareness in Organisationen implementiert werden kann
-                (Transformations-, Veränderungs- und Anpassungsprozesse in Organisationen initiieren, begleiten und
-                verankern) und zum anderen um einen Austausch darüber, welche Anwendungsbedingungen und Hindernisse in
-                den einzelnen Kultursparten bestehen und um die Erarbeitung von Gelingensfaktoren für die jeweiligen
-                Kultursparten.
-              </p>
-              <p>Bei weiteren Fragen stehen wir (Köllektive Awareness) gerne zur Verfügung.</p>
-            </div>
+            <!-- Logos -->
             <div class="flex justify-center">
               <NuxtImg src="/images/k-amt.png" class="object-contain h-20 rounded-3xl mt-4" alt="Unsere Mission" />
               <NuxtImg src="/images/logo.jpg" class="object-contain h-24 rounded-3xl ml-8" alt="Unsere Mission" />
-
             </div>
           </div>
+
+          <h4 style="margin-top: 120px;"  class="text-left text-xl font-bold mt-10">Und weiter gehts in der zweiten Jahreshälfte 2025 mit den Workshops 3 & 4!</h4>
+
+          <!-- Workshop 3 -->
+            <div style="margin-top: 120px;" class="text-left space-y-3 text-xl">
+              <h4 class="text-xl font-bold mt-10">3. Workshop: Awareness in Organisationen einführen und etablieren</h4>
+              <p>
+                Du möchtest Awareness als Haltung und Konzept in eine Einrichtung oder Organisation einbringen?<br>
+                Du befindest dich am Anfang dieser Auseinandersetzung und fragst dich, wie du Verbündete finden und Kolleg*innen an ihrem Standpunkt abholen kannst? <br>
+                Was ist dabei wichtig und wie kannst du angemessen mit deinen Ressourcen umgehen,
+                um nachhaltig Veränderung anzustoßen?<br>
+                Wie erkennst du, ob du mit deinen Impulsen erfolgreich bist?
+              </p>
+              <p>
+                Nach den Einführungsworkshops laden wir mit diesen Impulsen zu einer vertieften Auseinandersetzung dazu ein, wie Awareness in Organisationen etabliert werden kann.
+                Wir widmen uns außerdem den Fragen, wie wir in verschiedenen Räumen Unterstützer*innen finden und konstruktiv mit Widerständen umgehen können.
+              </p>
+              <p><strong>Zielgruppe:</strong> Engagierte Einzelpersonen oder Arbeitsgruppen mit Vorkenntnissen in Awareness, die in ihrer jeweiligen Organisation für Awareness eintreten.</p>
+              <p>
+                Der Workshop baut auf den Inhalten der vorherigen Workshops auf. Wir setzen deshalb die Teilnahme an mindestens einem unserer Einführungsworkshops oder ähnliche Kenntnisse voraus.
+              </p>
+              <p><strong>Themenauszug:<br>
+              Recap:</strong></p>
+              <ul class="m-0 !mt-0 !mb-0 list-disc ml-6">
+                <li>Awareness als Haltung und Konzept</li>
+                <li>Wertschätzende Kommunikation und Feedbackkultur</li>
+              </ul>
+              <p><strong>Vertiefung:</strong></p>
+              <ul class="m-0 !mt-0 !mb-0 list-disc ml-6">
+                <li>Von der individuellen zur gemeinsamen Vision</li>
+                <li>Verbündete finden</li>
+                <li>Widerstände transformieren, Backlash begegnen</li>
+                <li>Ressourcen schonen</li>
+                <li>Nachhaltige Veränderungen bewirken</li>
+                <li>Meilensteine setzen und erkennen</li>
+              </ul>
+              <p><strong>Wann?</strong> Sa., 08.11.2025 11 – 17 Uhr</p>
+              <p><strong>Wo?</strong> Wohngemeinschaft, Richard-Wagner-Str. 39, 50674 Köln</p>
+              <p><strong>Barriereinformationen:</strong><br>
+                Die Veranstaltung findet in deutscher Sprache statt.<br>
+                Die Räumlichkeiten sind für Rollstuhlnutzende zugänglich.<br>
+                Eine Verdolmetschung in Deutsche Gebärdensprache, Induktionstechnik oder Kinderbetreuung können wir leider nicht anbieten.
+              </p>
+              <p><strong><u>Anmeldung:</u></strong> Name und Kontext an <a class="underline font-bold"
+                  href="mailto:info@koellektive-awareness.de">info@koellektive-awareness.de</a></p>
+              <p>Gefördert wird die insgesamt vierteilige Workshopreihe von der Stadt Köln im Programm „Kultur – Diversity“. <strong>Die Teilnahme ist daher kostenfrei!</strong></p>
+
+              <!-- Logos -->
+              <div class="flex justify-center">
+                <NuxtImg src="/images/k-amt.png" class="object-contain h-20 rounded-3xl mt-4" alt="Unsere Mission" />
+                <NuxtImg src="/images/logo.jpg" class="object-contain h-24 rounded-3xl ml-8" alt="Unsere Mission" />
+              </div>
+            </div>
+
+
+            <!-- Workshop 4 -->
+            <div style="margin-top: 120px;" class="text-left space-y-3 text-xl">
+              <h4 class="text-xl font-bold mt-10">4. Workshop: Awareness in der Vielfalt kultureller Räume</h4>
+              <p>
+                Dem Aufbau einer Awareness-Struktur stehen oft institutionelle Hindernisse und begrenzte Ressourcen entgegen - das haben in der Regel alle Kultureinrichtungen gemeinsam.
+                Jedoch unterscheiden sich die Grundbedingungen einer Awareness-Strategie in einem Kino maßgeblich von den Voraussetzungen in einem Museum oder an einem Filmset.
+                Deshalb gehen wir in einem letzten Workshop auf die besonderen Herausforderungen bei der Anwendung von Awareness in verschiedenen Kultursparten ein.
+              </p>
+              <p>
+                Wir wollen uns dazu, gemeinsam mit euch, die spezifische Charakteristik eurer Kulturbereiche genauer anschauen und uns anschließend mit den Umsetzungsmöglichkeiten und Gelingensfaktoren in der jeweiligen Kulturorganisation beschäftigen.<br>
+                Außerdem freuen wir uns, in unserem letzten Workshop in dieser Reihe, am Ende auf einen entspannten Austausch und Raum fürs Netzwerken in der Bar der Wohngemeinschaft.
+              </p>
+              <p><strong>Zielgruppe:</strong> Engagierte Einzelpersonen oder Arbeitsgruppen mit Vorkenntnissen in Awareness, die in ihrer jeweiligen Organisation für Awareness eintreten. Teilnahmevoraussetzung sind Kenntnisse von und Auseinandersetzung mit den Basics von Awareness (Grundprinzipien von Awareness und der Betroffenenunterstützung, Privilegien und Diskriminierungsformen).</p>
+              <p><strong>Fragen, die wir uns stellen werden:</strong></p>
+              <ul class="m-0 !mt-0 !mb-0 list-disc ml-6">
+                <li>Was sind die strukturellen Besonderheiten eures Kulturbereichs?</li>
+                <li>Worauf sollte daher der Fokus eures Awareness Konzepts liegen?</li>
+                <li>Welche Awareness Tools passen zu euch?</li>
+                <li>Welche Mindeststandards müssen für “Awareness” erfüllt werden?</li>
+                <li>Wer ist noch in eurem Kulturbereich für Awareness unterwegs?</li>
+              </ul>
+              <p><strong>Wann?</strong>  Sa., 06.12.2025 <strong>- bitte beachten, 12 – 18 Uhr</strong></p>
+
+              <p><strong>Wo?</strong> Wohngemeinschaft, Richard-Wagner-Str. 39, 50674 Köln</p>
+              <p><strong>Barriereinformationen:</strong><br>
+                Die Veranstaltung findet in deutscher Sprache statt.<br>
+                Die Räumlichkeiten sind für Rollstuhlnutzende zugänglich.<br>
+                Eine Verdolmetschung in Deutsche Gebärdensprache, Induktionstechnik oder Kinderbetreuung können wir leider nicht anbieten.
+              </p>
+              <p><strong><u>Anmeldung:</u></strong> Bitte inkl. Namen, Pronomen und Kontext an <a class="underline font-bold"
+                  href="mailto:info@koellektive-awareness.de">info@koellektive-awareness.de</a></p>
+              <p>Gefördert wird die insgesamt vierteilige Workshopreihe von der Stadt Köln im Programm „Kultur – Diversity“. <strong>Die Teilnahme ist daher kostenfrei!</strong></p>
+
+              <!-- Logos -->
+              <div class="flex justify-center">
+                <NuxtImg src="/images/k-amt.png" class="object-contain h-20 rounded-3xl mt-4" alt="Unsere Mission" />
+                <NuxtImg src="/images/logo.jpg" class="object-contain h-24 rounded-3xl ml-8" alt="Unsere Mission" />
+              </div>
+            </div>
+
 
           <!-- 2024 Label (unchanged) -->
           <div class="border-2 border-solid border-black rounded-2xl w-full py-2 mt-24 lg:mt-48 mb-12">
